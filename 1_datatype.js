e6cdf56437750ea599431f641ebbef3c10701c7d
sumedhman <sumedh.manwatkar@gmail.com>
2023-07-26 15:26:05 +0530

    This is first program in typescript

---
+++ 1_datatype.js
@@ -0,0 +1,7 @@
+var x = 10;
+var y = 20;
+var last_name = "doe";
+var Trainer_name = "chinmoy";
+console.log(x);
+console.log(y);
+console.log(Trainer_name.toUpperCase());
---
+++ 1_datatype.ts
@@ -0,0 +1,12 @@
+var x:number=10
+var y:number=20
+
+
+var last_name:string="doe"
+
+var Trainer_name:string="chinmoy";
+console.log(x);
+console.log(y);
+console.log(Trainer_name.toUpperCase());
+
+
